888875ccf0329dbeb34de6087390a3ea793a20e7
Rodrigolms1 <rodrigolms@msn.com>
2025-12-16 21:09:35 -0300

    Update DashTreinamento.py

--- DashTreinamento.py
+++ DashTreinamento.py
@@ -1,36 +1,39 @@
 import streamlit as st # Construir Dashboards
 import pandas as pd # Ler arquivos
-#import plotly.express as px # Construir Gráficos
+import datetime # Manipular datas
+import plotly.express as px # Construir Gráficos
 
 st.header('NÚMEROS MENSAIS DA CAPELA SAGRADO CORAÇÃO DE JESUS') # Título do Dashboard
 
 st.markdown('# Números Mensais')
-st.caption("Despesas")
+st.caption("Demonstrativos")
 
 st.set_page_config(layout="wide") # Configura a página para o modo wide
 
 df=pd.read_excel('PAGAMENTOS.xlsx') # Lê o arquivo Excel
 df2=pd.read_excel('RECEBIMENTOS.xlsx') # Lê o arquivo Excel
-df['DATA']=pd.to_datetime(df['DATA']) # Converte a coluna DATA para datetime
 
-#st.bar_chart(df) # Gráfico de linha com os dados do DataFrame
-#if st.sidebar.button('Exibir Gráfico'):
-#    fig = px.bar(df, x='DATA', y='VALOR', title='Despesas Mensais') # Cria um gráfico de barras
-#    st.plotly_chart(fig) # Exibe o gráfico no Streamlit
 
-opcao=st.sidebar.selectbox(
-    'Selecione uma opção:',
-    ('Ver Recebimento', 'Ver Pagamentos','Ver Gráfico')
-)
+df['DATA']=df['DATA'].dt.date # Extrai apenas a data (sem hora)
+df2['DATA']=df2['DATA'].dt.date # Extrai apenas a data (
 
-if opcao == 'Ver Recebimento':
-    st.dataframe(df2) # Exibe o DataFrame como uma tabela interativa
-elif opcao == 'Ver Pagamentos':
-    #fig = px.line(df, x='DATA', y='VALOR', title='Despesas Mensais') # Cria um gráfico de linhas
-    #st.plotly_chart(fig) # Exibe o gráfico no Streamlit
-    st.dataframe(df) # Exibe o DataFrame como uma tabela interativa
-elif opcao == 'Ver Gráfico':
-    st.line_chart(df) # Cria um gráfico de barras
-    st.line_chart(df2) # Cria um gráfico de barras
-    #st.line_chart(fig) # Exibe o gráfico no Streamlit
-    #st.line_chart(fig2) # Exibe o gráfico no Streamlit
+df=df.sort_values(by='DATA', ascending=False) # Ordena o DataFrame pela coluna DATA em ordem decrescente
+
+df["Month"]=df["DATA"].apply(lambda x: str(x.year) + '-' + str(x.month)) # Extrai o mês e ano da coluna DATA
+df2["Month"]=df2["DATA"].apply(lambda x: str(x.year) + '-' + str(x.month)) # Extrai o mês e ano da coluna DATA
+
+month=st.sidebar.selectbox("Mês", df["Month"].unique()) # Cria um selectbox na barra lateral
+
+df_filtered=df[df["Month"]==month] # Filtra o DataFrame pelo mês selecionado
+#df2_filtered=df2[df2["Month"]==month] # Filtra o DataFrame pelo mês selecionado
+df_filtered
+
+col1, col2=st.columns(2) # Cria duas colunas
+col3, col4, col5=st.columns(3) # Cria mais três colunas
+
+fig_data=px.bar(df_filtered, x='DATA', y='VALOR', color='DESCRIÇÃO', title='Pagamentos') # Cria um gráfico de barras 
+col1.plotly_chart(fig_data, use_container_width=True) # Exibe o gráfico na coluna 1
+
+fig_cred=px.pie(df_filtered, names='CREDOR', values='VALOR',
+                title='Pagamentos por Credor') # Cria um gráfico de pizza 
+col2.plotly_chart(fig_cred) # Exibe o gráfico na coluna 2
